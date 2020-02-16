93f593f61555883f517752eefacf2edac1b0f59d
Matt Canty <matthewcanty@gmail.com>
2020-02-16 16:48:18 +0000

    Add a thought about testing... to do testing later (#11)

--- pkg/macos/helpers/defaults_test.go
+++ pkg/macos/helpers/defaults_test.go
@@ -1,1 +1,7 @@
 package macoshelpers
+
+// Hard to test. There is something interesting on the topic here:
+// https://npf.io/2015/06/testing-exec-command/
+//
+// Overall I don't see a value right now given the current configuration.
+// Running commands is staple of this project... needs thought!
